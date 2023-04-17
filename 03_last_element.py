6c509e2d3e9e796308fda5421e6bd38657318fb8
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-17 16:52:49 -0700

    initial commit + 10 problems

---
+++ 03_last_element.py
@@ -0,0 +1,17 @@
+def last_element(lst):
+    """Return last item in list (None if list is empty.
+
+        >>> nums = [1, 2, 3]
+        >>> last_element(nums)
+        3
+        >>> last_element([]) is None
+        True
+
+    Make sure original list has not been mutated:
+
+        >>> nums == [1, 2, 3]
+        True
+    """
+    if len(lst) == 0:
+        return None
+    return lst[-1]
